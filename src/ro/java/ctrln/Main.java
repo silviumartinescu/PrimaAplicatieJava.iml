6e6b99cd7ccebf430e28ce3520da94b58b8742c5
Silviu <martinescu.silviu28@gmail.com>
2021-01-26 19:47:06 +0200

    Adaugare comentarii pe o singura linie

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -3,11 +3,7 @@ package ro.java.ctrln;
 public class Main {
 
     // Aceasta este metoda main,care porneste o aplicatie Java
-    /*
-    Aici este un alt exemplu
-    de comentariu pe
-     mai multe linii
-     */
+
     public static void main(/* args este parametrul metodei main */String[] args) {
        /*Inainte de comentariu nu se poate pune text
         // Aici vrem sa afisam pe consola IntelliJ un mesaj ales de noi
