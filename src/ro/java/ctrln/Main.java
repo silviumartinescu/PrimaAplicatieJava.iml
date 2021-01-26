8e608d9f955b0f10ea495a65a859781e71108494
Silviu <martinescu.silviu28@gmail.com>
2021-01-26 18:48:09 +0200

    PrimaAplicatieJava

---
+++ src/ro/java/ctrln/Main.java
@@ -0,0 +1,11 @@
+package ro.java.ctrln;
+
+public class Main {
+
+    // Aceasta este metoda main,care porneste o aplicatie Java
+    public static void main(String[] args) {
+       // Inainte de comentariu nu se poate pune text
+        // Aici vrem sa afisam pe consola IntelliJ un mesaj ales de noi
+	    System.out.println("Invat Limbajul Java si despre masina virtuala Java!");
+    }
+}
